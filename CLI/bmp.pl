e59b7de7433ea4327f23e7f521029a5782e9cfaf
Vladislav Stepanov <8uk.8ak@gmail.com>
2011-06-05 01:12:30 +0400

    restructurize

---
+++ CLI/bmp.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use File::Copy;
+use English;
+do 'bmp_steg.pl';
+
+sub clear {
+	if ($OSNAME =~ m/win/i) { system "cls" }
+	else { system "clear"}
+}
+
+START:
+
+clear;
+print "1. Write to file\n2. Read from file\n\n0. Exit\n\nEnter what do you want to do: ";
+
+chomp ($choice = <>);
+
+our $time_s2b = 0;
+
+goto START if ($choice !~ /[0-2]{1}/);
+
+if ($choice == 1) {
+	clear;
+	print "Enter text (Ctrl-Z to stop):\n";
+	@text = <>;
+	$text = join ('', @text);
+	copy("in.bmp","out.bmp") or die "Copy failed: $!";
+	$time = time;
+	write2Bmp ($text, "out.bmp");
+	$time = time - $time;
+	print "Done in $time seconds.\n"
+}
+
+elsif ($choice == 2) {
+	print "Enter name of file: ";
+	chomp ($file=<>);
+	$time = time;
+	$text = readBmp ($file);
+	clear;
+	$time = time - $time;
+	print "There was:\n$text\n\nDone in $time seconds.\nstr2bin func used $time_s2b msecs";
+}
---
+++ CLI/wav.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use File::Copy;
+use English;
+do 'wav_steg.pl';
+
+sub clear {
+	if ($OSNAME =~ m/win/i) { system "cls" }
+	else { system "clear"}
+}
+
+START:
+
+clear;
+print "1. Write to file\n2. Read from file\n\n0. Exit\n\nEnter what do you want to do: ";
+
+chomp ($choice = <>);
+
+goto START if ($choice !~ /[0-2]{1}/);
+
+if ($choice == 1) {
+	clear;
+	print "Enter text (Ctrl-Z to stop):\n";
+	@text = <>;
+	$text = join ('', @text);
+	$time = time;
+	copy("in.wav","out.wav") or die "Copy failed: $!";
+	write2Wav ($text, "out.wav");
+	$time = time - $time;
+	print "Done in $time seconds.\n"
+}
+
+elsif ($choice == 2) {
+	# print "Enter name of file: ";
+	# chomp ($file=<>);
+	# $text = readWav ($file);
+	$time = time;
+	$text = readWav ("out.wav");
+	clear;
+	$time = time - $time;
+	print "There was:\n$text\n\nDone in $time seconds.\n";
+}
